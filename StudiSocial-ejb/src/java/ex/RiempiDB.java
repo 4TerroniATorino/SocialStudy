a338d5467282104a0dade45698d7429b05da4a9d
Daniele <Daniele@192.168.1.128>
2014-02-13 17:19:46 +0100

    Merge origin/master
    Conflicts:
    	StudiSocial-war/nbproject/build-impl.xml
    	StudiSocial-war/nbproject/genfiles.properties
    	StudiSocial-war/nbproject/project.properties
    	StudiSocial-war/nbproject/project.xml

--- StudiSocial-ejb/src/java/ex/RiempiDB.java
+++ StudiSocial-ejb/src/java/ex/RiempiDB.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ex;
 
 import ejb.GestoreCorso;
+import ejb.GestoreCorsoLocal;
+import ejb.GestoreLocation;
 import ejb.GestoreLocationLocal;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,35 +20,18 @@ import javax.naming.NamingException;
  * @author Michele
  */
 public class RiempiDB {
-    static GestoreCorso gestoreCorso = lookupGestoreCorsoBean1();
-    static GestoreLocationLocal gestoreLocation = lookupGestoreLocationLocal1();
-    
-    public static void main(String[] args)
-    {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        GestoreLocation gestoreLocation = new GestoreLocation();
+        GestoreCorso gestoreCorso = new GestoreCorso();
         
         gestoreLocation.addLocation("Location", "via Pessinetto", null, "dipartimento di informatica");
         gestoreCorso.addCorso("Reti", "reti", "243", "Sereno", 9, 2, 23, "informatica", gestoreLocation.getLocation(1L));
         gestoreCorso.addCorso("Fisica", "fisica", "432", "Maggiora", 6, 1, 15, "fisica", gestoreLocation.getLocation(1L));
     }
-    
-    private static GestoreLocationLocal lookupGestoreLocationLocal1() {
-        try {
-            Context c = new InitialContext();
-            return (GestoreLocationLocal) c.lookup("java:global/StudiSocial/StudiSocial-ejb/GestoreLocation!ejb.GestoreLocationLocal");
-        } catch (NamingException ne) {
-            throw new RuntimeException(ne);
-        }
-    }
 
-    private static GestoreCorso lookupGestoreCorsoBean1() {
-        try {
-            Context c = new InitialContext();
-            return (GestoreCorso) c.lookup("java:global/StudiSocial/StudiSocial-ejb/GestoreCorso!ejb.GestoreCorso");
-        } catch (NamingException ne) {
-            throw new RuntimeException(ne);
-        }
-    }
-
-    
-    
 }
